5eaa928b64bc82ed8a5a15c938d12153c9fcf335
Arodrigues537 <Alexandre.rodrigues@etudiant.univ-rennes.fr>
2025-09-26 10:53:58 +0200

    Branche test

--- fctTemps.m
+++ fctTemps.m
@@ -1,1 +1,9 @@
 %% Fonction test 1
+%% INITIALISATION =========================================================
+clear;close all;clc;
+
+% Fonction en temps
+for in=1:nmax
+    % T_ij, avec i=>n et j=>t
+    T(in,:)=an(in)*cos(wn(in)*t)+bn(in)*sin(wn(in)*t)/wn(in); 
+end
