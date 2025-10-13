880ac2a9f3c65fe117b4255ae3c4b70b37e923bf
Amyne <amynek19@gmail.com>
2025-10-13 10:34:28 +0200

    Ajout des options d'affichage dans fctTemporelle et modesPropre

--- FctTemporelle.m
+++ FctTemporelle.m
@@ -5,9 +5,12 @@ for in=1:Nw
     T(in,:)=an(in)*cos(wn(in)*t)+bn(in)*sin(wn(in)*t)/wn(in); 
 end
 
-%=> visualisation de T(t) pour quelques modes
-figure(3);
-plot(t,T([1:3 Nw],:),'LineWidth',2)
-xlabel('t [s]')
-legend('n=1','n=2','n=3','n=Nw')
-set(gca,'FontSize',24)
+switch Aff
+    case 0 % => Aucun affichage
+    case 1 % => visualisation de T(t) pour quelques modes
+        figure(3);
+        plot(t,T([1:3 Nw],:),'LineWidth',2)
+        xlabel('t [s]')
+        legend('n=1','n=2','n=3','n=Nw')
+        set(gca,'FontSize',24)
+end
--- ModePropre.m
+++ ModePropre.m
@@ -5,10 +5,13 @@ for in=1:Nw
     Y(in,:)=sin(kn(in)*s); 
 end
 
+switch Aff
+    case 0   % => Aucun affichage
+    case 1   % => visualisation de quelques modes propres
+        figure(1);
+        plot(s,Y([1:3 nmax],:),'LineWidth',2)
+        xlabel('s [m]')
+        legend('n=1','n=2','n=3','n=nmax')
+        set(gca,'FontSize',24)
+end
 
-%=> visualisation de quelques modes propres
-figure(1);
-plot(s,Y([1:3 nmax],:),'LineWidth',2)
-xlabel('s [m]')
-legend('n=1','n=2','n=3','n=nmax')
-set(gca,'FontSize',24)
