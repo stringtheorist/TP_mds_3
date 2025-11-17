c202842a5ac4daff9011c181045938617ff525c1
PaulCarlu <paulcarlu2908@gmail.com>
2025-11-17 11:17:48 +0100

    message

--- AmplitudeModale.m
+++ AmplitudeModale.m
@@ -1,20 +1,33 @@
-function [an,bn]=AmplitudeModale(L,el,kn,wn,n,H,Aff)
-
+function [an,bn]=AmplitudeModale(L,el,kn,wn,n,H,Aff,ConditionLimite ...
+    )
 
 % Amplitude modale
-an=2*H./(n*pi)*L/(L-el).*sin(kn*el)./(kn*el);
-bn=zeros(size(n));
-%=> visualisation des amplitudes modales an
+switch ConditionLimite
+    case 1 % Corde pincée
+        an = 2*H./(n*pi)*L/(L-el).*sin(kn*el)./(kn*el);
+        bn = zeros(size(n));
+    case 2 % Corde frappée
+        an = zeros(size(n));
+        bn = 2*h./(n*pi).*sin(kn*el)./(L*wn);
+end
 
-if (Aff>0)
-    figure(2);
-    stem(wn,abs(an),'LineWidth',2)
-    xlabel('wn [rad/s]')
-    ylabel('|an| [m]')
-    set(gca,'FontSize',24)
-
-else
-    disp('Aff dois etre un valeur positif');
+switch Aff
+    case 0 % Affiche rien
+    case 1 % visualisation des amplitudes modales an et bn
+        % an
+        figure(2);subplot(1,2,1);
+        stem(wn,abs(an),'LineWidth',2)
+        xlabel('wn [rad/s]')
+        ylabel('|an| [m]')
+        set(gca,'FontSize',24)
+        %bn
+        figure(2);subplot(1,2,2);
+        stem(wn,abs(bn),'LineWidth',2)
+        xlabel('wn [rad/s]')
+        ylabel('|an| [m]')
+        set(gca,'FontSize',24)
+    otherwise 
+        disp('Aff dois etre un valeur positif');
 end 
 
 
