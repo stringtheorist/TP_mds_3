fbfb72e904cfc94809be24331a0b3c82566d91aa
karolina <kochman04@gmail.com>
2025-11-17 10:43:18 +0100

    added explanation for Aff in ParamInit

--- ParamInit.m
+++ ParamInit.m
@@ -1,6 +1,10 @@
 function [L,R,E,ro,Note,H,el,Nw,Aff]=ParamInit
 %% INITIALISATION =========================================================
-Aff = [1,1,1];        % Affichage si Aff > 0, si non Aff = 0
+Aff = [1,1,1];        % 
+%Aff(1) pour ModePropre, affichage si Aff > 0, sinon Aff = 0
+%Aff(2) pour AmplitudeModale, affichage si Aff > 0, sinon Aff = 0
+%Aff(3) pour FctDeplacement, affichage si Aff > 0, sinon Aff = 0
+
 % Geometrie : section cicrculaire
 L=1;            % Longueur [m]
 R=0.001;        % Rayon [m]
