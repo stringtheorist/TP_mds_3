187d74bf8147a48029279ba09c4f0b56528b3473
Amyne <amynek19@gmail.com>
2025-11-17 11:02:43 +0100

    aff

--- ParamInit.m
+++ ParamInit.m
@@ -3,7 +3,7 @@ function [L,R,E,ro,Note,H,el,Nw,Aff]=ParamInit(Type)
 switch Type
     case 0
         %% INITIALISATION =========================================================
-        Aff = 1;        % Affichage si Aff > 0, si non Aff = 0
+        Aff = [1,1,1,1];        % Affichage si Aff > 0, si non Aff = 0
         % Geometrie : section cicrculaire
         L=1;            % Longueur [m]
         R=0.001;        % Rayon [m]
